1b9160704b71e7595b26c1248f875af242852018
manzyuball <kanerusanda3@gmail.com>
2022-11-07 20:51:11 +0900

    EoB.lua

---
+++ common/defines/01_EoB_defines.lua
@@ -0,0 +1,42 @@
+ --NDiplomacy
+ --____________________________________________________________
+NDefines.NDiplomacy.MAX_TRUST_VALUE = 200 	-- Max trust value cap.
+NDefines.NDiplomacy.MIN_TRUST_VALUE = -200,							-- Min trust value cap.
+NDefines.NDiplomacy.MAX_OPINION_VALUE = 200,						-- Max opinion value cap.
+NDefines.NDiplomacy.MIN_OPINION_VALUE = -100,						-- Min opinion value cap.
+NDefines.NDiplomacy.OPINION_PER_VOLUNTEER = 30,						-- Opinion bonus per one sent volunteer division
+NDefines.NDiplomacy.MAX_OPINION_FROM_VOLUNTEERS = 30,				-- Opinion bonus per one sent volunteer division
+--____________________________________________________________
+
+--NCountry
+--____________________________________________________________
+NDefines.NCountry.MIN_STABILITY = -2.0,
+NDefines.NCountry.MAX_STABILITY = 2.0,
+NDefines.NCountry.MIN_WAR_SUPPORT = -2.0,
+NDefines.NCountry.MAX_WAR_SUPPORT = 2.0,
+--____________________________________________________________
+
+--NTechnology
+--____________________________________________________________
+NDefines.NTechnology.BASE_RESEARCH_POINTS_SAVED = 60.0,
+--____________________________________________________________
+
+ --NBuildings
+ --____________________________________________________________
+NDefines.NBuildings.MAX_BUILDING_LEVELS = 35,
+NDefines.NBuildings.MAX_SHARED_SLOTS = 45,
+--____________________________________________________________
+
+--NMilitary
+--____________________________________________________________
+NDefines.NMilitary.HISTORY_OPERATION_RANDOM_MAX = 30
+NDefines.NMilitary.CORPS_COMMANDER_DIVISIONS_CAP = 30,
+NDefines.NMilitary.FIELD_MARSHAL_DIVISIONS_CAP = 30,
+NDefines.NMilitary.FIELD_MARSHAL_ARMIES_CAP = 5,
+NDefines.NMilitary.CORPS_COMMANDER_ARMIES_CAP = -1,
+NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 999,	
+NDefines.NMilitary.MAX_NAVY_EXPERIENCE = 999,
+NDefines.NMilitary.MAX_AIR_EXPERIENCE = 999,
+NDefines.NMilitary.TRAINING_MAX_LEVEL = 3,
+NDefines.NMilitary.DEPLOY_TRAINING_MAX_LEVEL = 1,
+--____________________________________________________________
